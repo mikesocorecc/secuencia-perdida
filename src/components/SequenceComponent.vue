765e000ebd661303e6c7578f69018cfbb5a21f30
mikesocorecc <mikesocorecqgmail.com>
2023-08-15 11:32:59 -0600

    Fix: Actualizar renderizado de números en la secuencia

--- src/components/SequenceComponent.vue
+++ src/components/SequenceComponent.vue
@@ -3,47 +3,31 @@
     <h1 class="text-2xl lg:text-4xl font-semibold text-center pt-5 font-montserrat">
       Completa la secuencia
     </h1>
-    <TransitionGroup
-      name="list"
-      tag="div"
-      class="flex relative md:static items-center justify-center my-10"
-    >
+    <TransitionGroup name="list" tag="div" class="flex relative md:static items-center justify-center my-10">
       <template v-for="(number, index) in sequence" :key="index">
         <h2
           class="flex justify-center items-center text-5xl font-montserrat md:relative w-[60px] h-[60px] md:w-[115px] md:h-28 lg:w-[135px] lg:h-32 text-center md:text-7xl lg:text-8xl font-semibold text-[#2B4457]"
-          :class="getMissingNumberClasses(number)"
-          :style="{ transitionDuration: index * 0.1 + 's' }"
-        >
-          {{ number ?? selectedOption }}
-          <OptionsValues
-            v-if="number == null && !isShowingResult"
-            :options="options"
-            @select-option="selectOption"
-          />
+          :class="getMissingNumberClasses(number)" :style="{ transitionDuration: index * 0.1 + 's' }">
+          <!-- {{ number ?? selectedOption }} -->
+          <template v-if="number !== null">
+            {{ number }}
+          </template>
+          <template v-else>
+            {{ selectedOption }}
+          </template>
+          <OptionsValues v-if="number == null && !isShowingResult" :options="options" @select-option="selectOption" />
           <OptionCorrect :isCorrect="isCorrect" :number="number" @select-option="selectOption" />
-          <OptionIncorrect
-            :isCorrect="isCorrect"
-            :number="number"
-            :missingValue="missingValue"
-            :isShowingResult="isShowingResult"
-            @select-option="selectOption"
-          />
+          <OptionIncorrect :isCorrect="isCorrect" :number="number" :missingValue="missingValue"
+            :isShowingResult="isShowingResult" @select-option="selectOption" />
         </h2>
-        <img
-          :style="{ transitionDuration: index * 0.1 + 's' }"
-          v-if="index !== sequence.length - 1"
-          class="grow-0 mx-2 w-6 md:w-14 lg:w-16 lg:mx-5"
-          src="../assets/arrow_right.svg"
-          alt="arrow_right.svg"
-        />
+        <img :style="{ transitionDuration: index * 0.1 + 's' }" v-if="index !== sequence.length - 1"
+          class="grow-0 mx-2 w-6 md:w-14 lg:w-16 lg:mx-5" src="../assets/arrow_right.svg" alt="arrow_right.svg" />
       </template>
     </TransitionGroup>
     <div class="text-center mt-20">
-      <button
-        :disabled="isShowingResult"
+      <button :disabled="isShowingResult"
         class="bg-[#495259] w-full md:w-2/4 lg:w-auto p-4 uppercase font-bold text-white rounded-2xl font-montserrat"
-        @click="submit()"
-      >
+        @click="submit()">
         Responder
       </button>
     </div>
@@ -73,9 +57,11 @@ const {
 .list-leave-active {
   transition: all 0.5s ease;
 }
+
 .list-enter-from {
   transform: translateX(-100%);
 }
+
 .list-leave-to {
   transform: translateX(0);
 }
